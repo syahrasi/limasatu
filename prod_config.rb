27b9a05379ac65f654ea1aa135922eb8cf9e2158
Syahrasi <syahrasi@gmail.com>
2011-06-25 23:39:47 +0800

    add gitignore and prod_config

--- prod_config.rb
+++ prod_config.rb
@@ -5,5 +5,5 @@ images_dir = "images"
 javascripts_dir = "js"
 relative_assets = true
 output_style = :nested
-#environment = :production
-sass_options = {:debug_info => true}
+environment = :production
+#sass_options = {:debug_info => true}
